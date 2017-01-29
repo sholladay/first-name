51a76aaf4aa41da14035d74755e3950f4dbfd07f
Seth Holladay <me@seth-holladay.com>
2017-01-29 10:10:04 -0500

    Imrove failure mode for no full name.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fullname = require('fullname');
 
 const firstName = () => {
     return fullname().then((name) => {
+        if (!name) {
+            return name;
+        }
         const spaceIndex = name.indexOf(' ');
         return spaceIndex > 0 ? name.substring(0, spaceIndex) : name;
     });
